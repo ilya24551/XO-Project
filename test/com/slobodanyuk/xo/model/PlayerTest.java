d679e23dd3bd12a995d4bc7a83704af1dbe1821e
Ilya <ilyaslobodanyuk1@gmail.com>
2021-05-13 21:48:44 +0300

    add PlayerTest

--- test/com/slobodanyuk/xo/model/PlayerTest.java
+++ test/com/slobodanyuk/xo/model/PlayerTest.java
@@ -1,4 +1,28 @@
+package com.slobodanyuk.xo.model;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PlayerTest {
-  
+
+    @Test
+    void getName() {
+        final String testValue = "Ilya";
+        final String expectedValue = "Ilya";
+
+        final Player player = new Player(testValue, null);
+        final String actualValue = player.getName();
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    void getFigure() {
+        final Figure testValue = Figure.X;
+        final Figure expectedValue = Figure.X;
+
+        final Player player = new Player(null, testValue);
+        final Figure actualValue = player.getFigure();
+        assertEquals(expectedValue, actualValue);
+    }
 }
